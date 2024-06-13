7c91d5dd61c7582c0c23e610ea8723871b270a76
Vinícius <matosvb@gmail.com>
2024-06-12 21:52:00 -0300

    Add headers

--- field.class.php
+++ field.class.php
@@ -1,5 +1,24 @@
 <?php
 
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <https://www.gnu.org/licenses/>.
+
+
+
+
+// Important:
 // Moodle stores the entries in the table `data_content`,
 // which has the columns `id` (id of the entry),
 // `fieldid` (id of the field in the `data_fields` table),
@@ -8,7 +27,6 @@
 // We use:
 // - `content` to store the query;
 // - `content1` to store the answer.
-
 
 
 
