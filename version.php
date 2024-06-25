643f242da557b817b9ce1d4c57546acfb401d703
Vinícius <matosvb@gmail.com>
2024-06-24 23:20:52 -0300

    Make answer provider address configurable

--- version.php
+++ version.php
@@ -18,11 +18,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024052901;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024062401;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023100400;        // Requires this Moodle version.
 $plugin->component = 'datafield_harpiainteraction';  // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_ALPHA;
 
 $plugin->dependencies = [
-    'local_harpiaajax' => 2024052901,
+    'local_harpiaajax' => 2024062401,
 ];
